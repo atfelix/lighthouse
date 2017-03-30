93347d72e0030d74cda520c2f940dc1fb0c29398
Adam Felix <adam.tyler.felix@gmail.com>
2017-03-30 15:05:16 -0400

    Assignment #1 (classical FizzBuzz) complete in C.

---
+++ FizzBuzz/FizzBuzz/main.c
@@ -0,0 +1,40 @@
+//
+//  main.c
+//  FizzBuzz
+//
+//  Created by Owner1 on 2017-03-14.
+//  Copyright © 2017 Adam Felix. All rights reserved.
+//
+
+#include <stdio.h>
+
+#define N 100
+
+void fizzbuzz(int n);
+
+int main(int argc, const char * argv[]) {
+    
+    int i;
+    
+    for (i = 1; i <= N; i++) {
+        fizzbuzz(i);
+    }
+    
+    return 0;
+}
+
+
+void fizzbuzz(int i) {
+    if (i % 15 == 0) {
+        printf("FizzBuzz\n");
+    }
+    else if (i % 3 == 0) {
+        printf("Fizz\n");
+    }
+    else if (i % 5 == 0) {
+        printf("Buzz\n");
+    }
+    else {
+        printf("%d\n", i);
+    }
+}
